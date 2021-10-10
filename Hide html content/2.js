db303da6bfb615f316902a60444b62c478a32fb2
Aboubaker Riche <daging129@gmail.com>
2021-10-10 13:05:05 +0100

    first node js

---
+++ Hide html content/2.js
@@ -0,0 +1,8 @@
+function Show() { 
+	let elem = document.getElementById('Khenchela')
+	elem.style.display ='block'
+	}
+function Hide() {
+	let elem = document.getElementById('Khenchela')
+	elem.style.display = 'none'
+}
